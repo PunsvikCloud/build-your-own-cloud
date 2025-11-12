f609f797c5f8700a2ec3aabac1976cd29e391ae7
Bjørn Kristian Punsvik <bjorn.punsvik@enova.no>
2025-11-12 09:48:34 +0100

    feat: init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace build_your_own_cloud.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
